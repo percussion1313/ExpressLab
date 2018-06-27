9539cdf6d4e82d4922cab6484e5eeadfd419e73c
percussion1313 <peterjacobgrant@gmail.com>
2018-06-26 22:44:08 -0500

    Added css to h1 and functionality

--- server/server.js
+++ server/server.js
@@ -4,15 +4,14 @@ let app = express();
 
 
 app.use((req, res, next) => {
-    console.log(req.url);
+    console.log(req.originalUrl);
     next(); 
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello from the web server side...')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello from the web server side...')
+// })
 
 app.use(express.static(path.join(__dirname, '../public')));
-
 
 app.listen(3000);
